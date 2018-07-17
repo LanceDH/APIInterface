bd410da79b4cd19a60cf863f871220d8b078d012
LanceDH <kiep_out@hotmail.com>
2018-07-17 11:48:49 +0200

    8.0.01b
    What appears to be a long overdue sync

--- APIInterface.lua
+++ APIInterface.lua
@@ -3,50 +3,78 @@ local _addonName, _addon = ...;
 
 local APII = LibStub("AceAddon-3.0"):NewAddon(_addonName);
 
-
 local LISTITEM_HEIGHT = 34;
 local LISTITEM_EXPAND_MARGIN = 43;
-local ERROR_COMBAT = "Can't open %s during combat. The frame will open once you leave combat."
+local SEARCH_CUTOFF_AMOUNT = 1000;
+local FORMAT_SEARCH_CUTOFF_CHANGED = "Search cutoff changed to %d for this session.";
+local FORMAT_SEARCH_CUTOFF = "Searched stopped after %d results."
+local FORMAT_NO_RESULTS = "No global variables found containing \"%s\"."
+local FORMAT_RESULTS_TITLE = "%d %ss containing \"%s\"";
+local FORMAT_IN_SYSTEMS = "In system: %s";
+local TOOLTIP_GLOBAL_FORMAT = "Search for global variables for %ss containing the given search string.";
+local TOOLTIP_GLOBAL_FRAME = "Searching for functions can cause errors trying to access forbidden variables.";
+local TOOLTIP_GLOBAL_STRING1 = "Hold shift to search in both variable name and value.";
+local TOOLTIP_GLOBAL_STRING2 = "Hold control to search only in variable value.";
+local ERROR_COMBAT = "Can't open %s during combat. The frame will open once you leave combat.";
+local COLOR_FILTER1 = "|cff......";
+local COLOR_FILTER2 = "|r";
+local DETAILS_NO_PUBLIC = "This function does nothing in public clients";
+local DETAILS_NO_PUBLIC_REPLACE = "|cffff0000This function does nothing in public clients|r";
 ----------
 -- Code
 ----------
 
+local function APII_Resize_OnMouseUp(self, button)
+	if (button == "LeftButton") then
+		APII_Core:StopMovingOrSizing();
+		HybridScrollFrame_CreateButtons(APIIListsSystemList, "APII_ListSystemTemplate", 1, 0);
+		APIIListsSystemListScrollBar.doNotHide = true;
+		APII:UpdateSystemList();
+		APII:AdjustSelection();
+	end
+end
+
+local function APII_Resize_OnMouseDown(self, button)
+	if (button == "RightButton") then
+		APII:ResetFrame()
+	elseif (button == "LeftButton") then
+		APII_Core:StartSizing();
+	end
+end
+
 function APII_List_OnClick(self, button)
-	if (self.Type ~= "system" ) then -- and APIIListsSystemList.Opened ~= self.Api:GetFullName()
+	if (self.Type ~= "system" ) then
 		if ( self.selected ) then
 			self.selected = nil;
 			APIIListsSystemList.Opened = "";
 			self:SetHeight(LISTITEM_HEIGHT);
 			HybridScrollFrame_CollapseButton(APIIListsSystemList);
+			--APIIListsSystemListScrollBar:SetValue(0);
 			APII:UpdateSystemList();
 		else
-		APIIListsSystemList.Opened = self.Api:GetFullName();
-		self.Details:SetText(table.concat(self.Api:GetDetailedOutputLines(), "\n", 2));
-		self:SetHeight(LISTITEM_HEIGHT + LISTITEM_EXPAND_MARGIN + self.Details:GetHeight());
-		HybridScrollFrame_ExpandButton(APIIListsSystemList, ((self.index - 1) * LISTITEM_HEIGHT), self:GetHeight());
-		APII:UpdateSystemList();
-		APII:AdjustSelection();
+			APIIListsSystemList.Opened = self.Api:GetFullName();
+			self.Details:SetText(table.concat(self.Api:GetDetailedOutputLines(), "\n", 2));
+			self:SetHeight(LISTITEM_HEIGHT + LISTITEM_EXPAND_MARGIN + self.Details:GetHeight());
+			HybridScrollFrame_ExpandButton(APIIListsSystemList, ((self.index - 1) * LISTITEM_HEIGHT), self:GetHeight());
+			APII:UpdateSystemList();
+			APII:AdjustSelection();
+			
 		end
 	else
 		APIIListsSystemList.InSystem = self.Api;
 		APII_UpdateFilterBar()
 		APIILists.searchBox:SetText("");
+		APIIListsSystemListScrollBar:SetValue(0);
 	end
 	
-	if (CopyToClipboard and button == "RightButton" and self.Api) then
-		local clipboardString = self.Api:GetClipboardString();
-		CopyToClipboard(clipboardString);
-		print(("Copied to clipboard: %s"):format(clipboardString));
-	end
+	PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
 end
 
-function UpdateSearchResults()
+function APII:UpdateSearchResults()
 	for i=#APIIListsSystemList.SearchResults, 1, -1 do
 		table.remove(APIIListsSystemList.SearchResults, i)
 	end
 
-	--if (APIIListsSystemList.SearchString == "") then return; end
-	
 	local results = APIIListsSystemList.SearchResults;
 	local matches;
 	if APIIListsSystemList.InSystem then
@@ -71,23 +99,50 @@ function UpdateSearchResults()
 		table.insert(results, info);
 	end
 	
+	for k, info in ipairs(matches.events) do
+		table.insert(results, info);
+	end
+	
 	for k, info in ipairs(matches.tables) do
 		table.insert(results, info);
 	end
 
 end
 
-function APII_Search_OnTextChanged(self)
-	local searchString = self:GetText();
+local function IsSearchMatch(key, value, search, objType, wantFrames)
+	if (type(value) ~= objType) then return false; end
+	if objType.GetDebugName and objType:IsProtected() then return false; end
+	if (objType == "table") then
+		if wantFrames then
+			return value.GetDebugName  ~= nil and key:lower():find(search:lower());
+		end
+		return value.GetDebugName  == nil and key:lower():find(search:lower());
+	end
+	
+	if (objType == "string") then
+		if IsShiftKeyDown() then
+			return key:lower():find(search:lower()) or value:lower():find(search:lower());
+		elseif IsControlKeyDown() then
+			return value:lower():find(search:lower());
+		end
+	end
+
+	return key:lower():find(search:lower()) 
+end
+
+APII_COREMIXIN = {};
+
+function APII_COREMIXIN:Search_OnTextChanged(searchBox)
+	local searchString = searchBox:GetText();
 	
 	-- Make 'Search' text disapear when needed
-	SearchBoxTemplate_OnTextChanged(self);
+	SearchBoxTemplate_OnTextChanged(searchBox);
 	-- protect against malformed pattern
 	if not pcall(function() APIDocumentation:FindAllAPIMatches(searchString) end) then 
-		self:SetTextColor(1, 0.25, 0.25, 1);
+		searchBox:SetTextColor(1, 0.25, 0.25, 1);
 		return; 
 	else
-		self:SetTextColor(1, 1, 1, 1);
+		searchBox:SetTextColor(1, 1, 1, 1);
 	end
 	
 	APIIListsSystemListScrollBar:SetValue(0);
@@ -95,17 +150,81 @@ function APII_Search_OnTextChanged(self)
 	APIIListsSystemList.SearchString = searchString;
 	APII:ResetListButtons();
 	
-	UpdateSearchResults()
+	APII:UpdateSearchResults()
 	APII:UpdateSystemList();
 	
+	APIILists.buttonFunctions:SetEnabled(searchString ~= "");
+	APIILists.buttonTables:SetEnabled(searchString ~= "");
+	APIILists.buttonFrames:SetEnabled(searchString ~= "");
+	APIILists.buttonStrings:SetEnabled(searchString ~= "");
+end
+
+function APII_COREMIXIN:GlobalSearch_OnClick(objType, wantFrames)
+	local search = APIIListsSystemList.SearchString;
+	local numResults = 0;
+	if (search == "") then return end;
+	
+	local results={};
+	for k,v in pairs(_G)do
+		if (IsSearchMatch(k, v, search, objType, wantFrames)) then 
+			results[k] = v;
+			numResults = numResults + 1;
+			if (numResults >= SEARCH_CUTOFF_AMOUNT) then
+				break;
+			end
+		end 
+	end;
+	
+	if numResults > 0 then
+		UIParentLoadAddOn("Blizzard_DebugTools");
+		DisplayTableInspectorWindow(results, FORMAT_RESULTS_TITLE:format(numResults, objType, search));
+		if (numResults >= SEARCH_CUTOFF_AMOUNT) then
+			print(FORMAT_SEARCH_CUTOFF:format(SEARCH_CUTOFF_AMOUNT));
+		end
+	else
+		print(FORMAT_NO_RESULTS:format(search));
+	end
+	
+	PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
+end
+
+function APII_COREMIXIN:GlobalSearch_OnEnter(frame, objType)
+	GameTooltip:SetOwner(frame, "ANCHOR_RIGHT");
+	GameTooltip:SetText("Search in _G");
+	GameTooltip:AddLine(TOOLTIP_GLOBAL_FORMAT:format(objType), 1, 1, 1);
+	if (objType == "frame") then
+		GameTooltip:AddLine(TOOLTIP_GLOBAL_FRAME, 1, 0.25, 0.25);
+	elseif (objType == "string") then
+		GameTooltip:AddLine(TOOLTIP_GLOBAL_STRING1, 1, 1, 1);
+		GameTooltip:AddLine(TOOLTIP_GLOBAL_STRING2, 1, 1, 1);
+	end
+	GameTooltip:Show();
+end
+
+function APII_COREMIXIN:FilterClearButton_OnClick(self)
+	APIIListsSystemListScrollBar:SetValue(0);	
+	APIIListsSystemList.InSystem = nil;
+	HybridScrollFrame_CollapseButton(APIIListsSystemList);
+	APII_UpdateFilterBar();
+	PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
+end
+
+function APII:ResetFrame()
+	APII_Core:SetSize(550, 450);
+	HybridScrollFrame_CreateButtons(APIIListsSystemList, "APII_ListSystemTemplate", 1, 0);
+	APIIListsSystemListScrollBar.doNotHide = true;
+	APII:UpdateSystemList();
+	APII:AdjustSelection();
 end
 
 function APII:FindSelection()
 	local _, maxVal = APIIListsSystemListScrollBar:GetMinMaxValues();
 	local scrollHeight = APIIListsSystemList:GetHeight();
 	local newHeight = 0;
+
 	APIIListsSystemListScrollBar:SetValue(0);	
-	while ( true ) do
+	local i = 0;
+	while (true) do
 		for _, button in next, APIIListsSystemList.buttons do
 			if ( button.selected ) then
 				newHeight = APIIListsSystemListScrollBar:GetValue() + APIIListsSystemList:GetTop() - button:GetTop();
@@ -114,18 +233,22 @@ function APII:FindSelection()
 				return;
 			end
 		end		
-		if ( APIIListsSystemListScrollBar:GetValue() == maxVal ) then		
-			return;
-		else
 			newHeight = newHeight + scrollHeight;
 			newHeight = min(newHeight, maxVal);
 			APIIListsSystemListScrollBar:SetValue(newHeight);
+		
+		if ( APIIListsSystemListScrollBar:GetValue() == maxVal or APIIListsSystemListScrollBar:GetValue() == 0 ) then	
+			-- If we don't actually find any selected button, reset scrollbar to the top
+			APIIListsSystemListScrollBar:SetValue(0);	
+			return;
 		end
 	end
 end
 
 function APII:AdjustSelection()
+	if APIIListsSystemList.Adjusting then return; end
 	local selectedButton;	
+	APIIListsSystemList.Adjusting = true;
 	-- check if selection is visible
 	for _, button in next, APIIListsSystemList.buttons do
 		if ( button.selected ) then
@@ -144,9 +267,7 @@ function APII:AdjustSelection()
 			if ( selectedButton:GetHeight() > APIIListsSystemList:GetHeight() ) then
 				newHeight = APIIListsSystemListScrollBar:GetValue() + APIIListsSystemList:GetTop() - selectedButton:GetTop();
 			else
-				
 				newHeight = APIIListsSystemListScrollBar:GetValue() + APIIListsSystemList:GetBottom() - selectedButton:GetBottom();
-				print(APIIListsSystemListScrollBar:GetValue(),APIIListsSystemList:GetBottom(),selectedButton:GetBottom(), newHeight)
 			end
 		end
 		if ( newHeight ) then
@@ -155,6 +276,7 @@ function APII:AdjustSelection()
 			APIIListsSystemListScrollBar:SetValue(newHeight);					
 		end
 	end
+	APIIListsSystemList.Adjusting = false;
 end
 
 function APII:ResetListButtons()
@@ -165,7 +287,7 @@ end
 
 function APII_UpdateFilterBar()
 	APIILists.filterBar:SetHeight(APIIListsSystemList.InSystem and 20 or 0.1);
-	APIILists.filterBar.text:SetText(APIIListsSystemList.InSystem and "In system: " .. APIIListsSystemList.InSystem.Name or "")
+	APIILists.filterBar.text:SetText(APIIListsSystemList.InSystem and FORMAT_IN_SYSTEMS:format(APIIListsSystemList.InSystem.Name) or "")
 	APIILists.filterBar.clearButton:SetShown(APIIListsSystemList.InSystem)
 	APII:UpdateSystemList()
 end
@@ -176,8 +298,8 @@ function APII:UpdateSystemList()
 	local buttons = scrollFrame.buttons;
 	if buttons == nil then return; end
 	
-	UpdateSearchResults();
-	local list = #APIIListsSystemList.SearchResults == 0 and APIDocumentation.systems or APIIListsSystemList.SearchResults;
+	APII:UpdateSearchResults();
+	local list = (APIIListsSystemList.SearchString == "" and  #APIIListsSystemList.SearchResults == 0) and APIDocumentation.systems or APIIListsSystemList.SearchResults;
 
 	for i=1, #buttons do
 		local button = buttons[i];
@@ -198,7 +320,8 @@ function APII:UpdateSystemList()
 		if ( displayIndex <= #list) then
 			button:Show();
 			local info = list[displayIndex];
-			button.Name:SetText(info:GetSingleOutputLine());
+			--button.Name:SetText(info:GetSingleOutputLine():gsub(COLOR_FILTER1, ""):gsub(COLOR_FILTER2, ""));
+			button.Name:SetText(info:GetSingleOutputLine())
 			button.Api = info;
 			button.Key = displayIndex;
 			button.Type = info:GetType();
@@ -207,11 +330,14 @@ function APII:UpdateSystemList()
 				button.selected = true;
 				
 				button.ClipboardString:Show();
-				button.ClipboardString:SetText(info:GetClipboardString());
+				button.ClipboardString:SetText(info.LiteralName and info.LiteralName or  info:GetClipboardString());
 				button.ClipboardInfo:Show();
 				button.Details:Show();
 				button.highlight:Show();
-				button.Details:SetText(table.concat(info:GetDetailedOutputLines(), "\n", 2));
+				local details =  table.concat(info:GetDetailedOutputLines(), "\n", 2)
+				details = details:gsub(DETAILS_NO_PUBLIC, DETAILS_NO_PUBLIC_REPLACE);
+				
+				button.Details:SetText(details);
 				button:SetHeight(LISTITEM_HEIGHT + LISTITEM_EXPAND_MARGIN + button.Details:GetHeight());
 			end
 		else
@@ -226,7 +352,14 @@ function APII:UpdateSystemList()
 end
 
 function APII:OnInitialize()
-
+	APII_Core:SetScript("OnDragStart", function(self)
+					self:StartMoving();
+				end
+			);
+	APII_Core:SetScript("OnDragStop", function(self)
+					self:StopMovingOrSizing();
+				end
+			);
 end
 
 function APII:OnEnable()
@@ -237,6 +370,7 @@ function APII:OnEnable()
 	HybridScrollFrame_CreateButtons(APIIListsSystemList, "APII_ListSystemTemplate", 1, 0);
 	HybridScrollFrame_Update(APIIListsSystemList, #APIDocumentation.systems * LISTITEM_HEIGHT, APIIListsSystemList:GetHeight());
 	APIIListsSystemListScrollBar.doNotHide = true;
+	APIIListsSystemListScrollBar:Show();
 	
 	APIIListsSystemList.update = function() APII:UpdateSystemList() end;
 	APIIListsSystemList.SearchString = "";
@@ -272,11 +406,20 @@ end
 SLASH_APIISLASH1 = '/apii';
 SLASH_APIISLASH2 = '/apiinterface';
 local function slashcmd(msg, editbox)
-	if (not InCombatLockdown()) then
-		ShowUIPanel(APII_Core);
+	if (msg == "reset") then
+		APII:ResetFrame();
+	elseif msg:match("^limit ") then
+		local amount = tonumber(msg:match("limit (%d+)"));
+		if not amount or amount < 1 then return; end
+		SEARCH_CUTOFF_AMOUNT = amount;
+		print(FORMAT_SEARCH_CUTOFF_CHANGED:format(SEARCH_CUTOFF_AMOUNT));
 	else
-		print(ERROR_COMBAT:format(_addonName));
-		APII.openDuringCombat = true;
+		if (not InCombatLockdown()) then
+			ShowUIPanel(APII_Core);
+		else
+			print(ERROR_COMBAT:format(_addonName));
+			APII.openDuringCombat = true;
+		end
 	end
 end
 SlashCmdList["APIISLASH"] = slashcmd
@@ -340,7 +483,6 @@ SlashCmdList["APIISLASH"] = slashcmd
 				-- table.remove(self.history, 1)
 			-- end
 			-- ShowDebugHistory()
-
 		-- end
 	-- end)
 -- l_debug:Show()
