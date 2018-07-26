24288c2ff324f7d0f9e3420aaf7403f7ab268f40
LanceDH <kiep_out@hotmail.com>
2018-07-26 14:18:45 +0200

    8.0.02
    Can now click on api names in descriptions to jump to that api
    Clicking on system, and then going back, remembers the position in the
    list
    Clearing searchbox focuson certain actions

--- APIInterface.lua
+++ APIInterface.lua
@@ -43,25 +43,26 @@ local function APII_Resize_OnMouseDown(self, button)
 end
 
 function APII_List_OnClick(self, button)
+	APIILists.searchBox:ClearFocus();
+
 	if (self.Type ~= "system" ) then
 		if ( self.selected ) then
 			self.selected = nil;
 			APIIListsSystemList.Opened = "";
 			self:SetHeight(LISTITEM_HEIGHT);
 			HybridScrollFrame_CollapseButton(APIIListsSystemList);
-			--APIIListsSystemListScrollBar:SetValue(0);
 			APII:UpdateSystemList();
 		else
 			APIIListsSystemList.Opened = self.Api:GetFullName();
 			self.Details:SetText(table.concat(self.Api:GetDetailedOutputLines(), "\n", 2));
 			self:SetHeight(LISTITEM_HEIGHT + LISTITEM_EXPAND_MARGIN + self.Details:GetHeight());
-			HybridScrollFrame_ExpandButton(APIIListsSystemList, ((self.index - 1) * LISTITEM_HEIGHT), self:GetHeight());
 			APII:UpdateSystemList();
 			APII:AdjustSelection();
 			
 		end
 	else
 		APIIListsSystemList.InSystem = self.Api;
+		APIIListsSystemList.SystemScroll = APIIListsSystemListScrollBar:GetValue();
 		APII_UpdateFilterBar()
 		APIILists.searchBox:SetText("");
 		APIIListsSystemListScrollBar:SetValue(0);
@@ -132,9 +133,37 @@ end
 
 APII_COREMIXIN = {};
 
-function APII_COREMIXIN:Search_OnTextChanged(searchBox)
-	local searchString = searchBox:GetText();
+function APII_COREMIXIN:HandleHyperlink(self, link, text, button)
+	local apiType, name, system = link:match("api:(%a+):(%a+):(%a*):?") ;
+	local apiInfo = APIDocumentation:FindAPIByName(apiType, name, system);
 	
+	if apiType == "system"  and APIIListsSystemList.InSystem ~= apiInfo then
+		APIIListsSystemList.InSystem = apiInfo
+		APII_UpdateFilterBar()
+		APIILists.searchBox:SetText("");
+		APIIListsSystemListScrollBar:SetValue(0);
+	elseif apiType == "table" then
+		
+		APIIListsSystemList.InSystem = apiInfo.System;
+		APII_UpdateFilterBar()
+
+		APIIListsSystemList.Opened = apiInfo:GetFullName();
+		
+		if 	APIILists.searchBox:GetText() == "" then
+			-- If we open something that is currently visible
+			APII:UpdateSystemList();
+			APII:AdjustSelection();
+		else
+			-- Search_OnTextChanged handles the list update
+			APIILists.searchBox:SetText("");	
+		end
+		
+	end
+end
+
+function APII_COREMIXIN:Search_OnTextChanged(searchBox, userInput)
+	local searchString = searchBox:GetText();
+	APIIListsSystemList.SearchString = searchString;
 	-- Make 'Search' text disapear when needed
 	SearchBoxTemplate_OnTextChanged(searchBox);
 	-- protect against malformed pattern
@@ -144,14 +173,16 @@ function APII_COREMIXIN:Search_OnTextChanged(searchBox)
 	else
 		searchBox:SetTextColor(1, 1, 1, 1);
 	end
+
 	
-	APIIListsSystemListScrollBar:SetValue(0);
+	if (userInput) then
+		APIIListsSystemListScrollBar:SetValue(0);
+		APII:ResetListButtons();
 	
-	APIIListsSystemList.SearchString = searchString;
-	APII:ResetListButtons();
-	
-	APII:UpdateSearchResults()
+		APII:UpdateSearchResults();
+	end
 	APII:UpdateSystemList();
+	APII:AdjustSelection();
 	
 	APIILists.buttonFunctions:SetEnabled(searchString ~= "");
 	APIILists.buttonTables:SetEnabled(searchString ~= "");
@@ -185,6 +216,7 @@ function APII_COREMIXIN:GlobalSearch_OnClick(objType, wantFrames)
 		print(FORMAT_NO_RESULTS:format(search));
 	end
 	
+	APIILists.searchBox:ClearFocus();
 	PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
 end
 
@@ -207,6 +239,7 @@ function APII_COREMIXIN:FilterClearButton_OnClick(self)
 	HybridScrollFrame_CollapseButton(APIIListsSystemList);
 	APII_UpdateFilterBar();
 	PlaySound(SOUNDKIT.IG_MAINMENU_OPTION_CHECKBOX_ON);
+	APIIListsSystemListScrollBar:SetValue(APIIListsSystemList.SystemScroll or 0);
 end
 
 function APII:ResetFrame()
@@ -214,42 +247,36 @@ function APII:ResetFrame()
 	HybridScrollFrame_CreateButtons(APIIListsSystemList, "APII_ListSystemTemplate", 1, 0);
 	APIIListsSystemListScrollBar.doNotHide = true;
 	APII:UpdateSystemList();
-	APII:AdjustSelection();
 end
 
 function APII:FindSelection()
-	local _, maxVal = APIIListsSystemListScrollBar:GetMinMaxValues();
 	local scrollHeight = APIIListsSystemList:GetHeight();
 	local newHeight = 0;
-
-	APIIListsSystemListScrollBar:SetValue(0);	
-	local i = 0;
-	while (true) do
-		for _, button in next, APIIListsSystemList.buttons do
-			if ( button.selected ) then
-				newHeight = APIIListsSystemListScrollBar:GetValue() + APIIListsSystemList:GetTop() - button:GetTop();
-				newHeight = min(newHeight, maxVal);
-				APIIListsSystemListScrollBar:SetValue(newHeight);					
-				return;
-			end
-		end		
-			newHeight = newHeight + scrollHeight;
-			newHeight = min(newHeight, maxVal);
-			APIIListsSystemListScrollBar:SetValue(newHeight);
-		
-		if ( APIIListsSystemListScrollBar:GetValue() == maxVal or APIIListsSystemListScrollBar:GetValue() == 0 ) then	
-			-- If we don't actually find any selected button, reset scrollbar to the top
-			APIIListsSystemListScrollBar:SetValue(0);	
-			return;
+	local info;
+	
+	-- Searching backwards as we are most likely looking for a table, which are at the back = less looping
+	for i=#APII.currentList, 1, -1 do
+		info = APII.currentList[i];
+		if info:GetFullName() == APIIListsSystemList.Opened then
+			return (i-1) * LISTITEM_HEIGHT;
 		end
 	end
+	
+	-- for k, v in ipairs(APII.currentList) do
+		-- if v:GetFullName() == APIIListsSystemList.Opened then
+			-- return (k-1) * LISTITEM_HEIGHT;
+		-- end
+	-- end
+	
+	return nil;
 end
 
 function APII:AdjustSelection()
-	if APIIListsSystemList.Adjusting then return; end
+	if APIIListsSystemList.Opened == "" or APIIListsSystemList.Adjusting then return; end
 	local selectedButton;	
 	APIIListsSystemList.Adjusting = true;
-	-- check if selection is visible
+	local newHeight;
+	--check if selection is visible
 	for _, button in next, APIIListsSystemList.buttons do
 		if ( button.selected ) then
 			selectedButton = button;
@@ -258,9 +285,10 @@ function APII:AdjustSelection()
 	end	
 	
 	if ( not selectedButton ) then
-		APII:FindSelection();
+		-- Not one of the buttons visible, search in the list
+		newHeight = APII:FindSelection();
 	else
-		local newHeight;
+		-- One of the visible buttons, make it fit in the display window
 		if ( selectedButton:GetTop() > APIIListsSystemList:GetTop() ) then
 			newHeight = APIIListsSystemListScrollBar:GetValue() + APIIListsSystemList:GetTop() - selectedButton:GetTop();
 		elseif ( selectedButton:GetBottom() < APIIListsSystemList:GetBottom() ) then
@@ -270,18 +298,21 @@ function APII:AdjustSelection()
 				newHeight = APIIListsSystemListScrollBar:GetValue() + APIIListsSystemList:GetBottom() - selectedButton:GetBottom();
 			end
 		end
-		if ( newHeight ) then
-			local _, maxVal = APIIListsSystemListScrollBar:GetMinMaxValues();
-			newHeight = min(newHeight, maxVal);
-			APIIListsSystemListScrollBar:SetValue(newHeight);					
-		end
 	end
+	
+	-- Set new height if any
+	if ( newHeight ) then
+		local _, maxVal = APIIListsSystemListScrollBar:GetMinMaxValues();
+		newHeight = min(newHeight, maxVal);
+		APIIListsSystemListScrollBar:SetValue(newHeight);				
+	end
+	
 	APIIListsSystemList.Adjusting = false;
 end
 
 function APII:ResetListButtons()
 	HybridScrollFrame_CollapseButton(APIIListsSystemList);
-	APIIListsSystemList.Opened = nil;
+	APIIListsSystemList.Opened = "";
 	APIIListsSystemList.ExpandedHeight = nil;
 end
 
@@ -297,10 +328,11 @@ function APII:UpdateSystemList()
 	local offset = HybridScrollFrame_GetOffset(scrollFrame);
 	local buttons = scrollFrame.buttons;
 	if buttons == nil then return; end
-	
 	APII:UpdateSearchResults();
-	local list = (APIIListsSystemList.SearchString == "" and  #APIIListsSystemList.SearchResults == 0) and APIDocumentation.systems or APIIListsSystemList.SearchResults;
-
+	local list = (APIIListsSystemList.SearchString == "" and #APIIListsSystemList.SearchResults == 0) and APIDocumentation.systems or APIIListsSystemList.SearchResults;
+	APII.currentList = list;
+	local heightStuff = LISTITEM_HEIGHT;
+	
 	for i=1, #buttons do
 		local button = buttons[i];
 		local displayIndex = i + offset;
@@ -320,35 +352,44 @@ function APII:UpdateSystemList()
 		if ( displayIndex <= #list) then
 			button:Show();
 			local info = list[displayIndex];
-			--button.Name:SetText(info:GetSingleOutputLine():gsub(COLOR_FILTER1, ""):gsub(COLOR_FILTER2, ""));
 			button.Name:SetText(info:GetSingleOutputLine())
 			button.Api = info;
 			button.Key = displayIndex;
 			button.Type = info:GetType();
 			button.index = displayIndex;
-			if scrollFrame.Opened == info:GetFullName()  then
+			if info.Type ~= "System" and scrollFrame.Opened == info:GetFullName()  then
 				button.selected = true;
-				
 				button.ClipboardString:Show();
 				button.ClipboardString:SetText(info.LiteralName and info.LiteralName or  info:GetClipboardString());
 				button.ClipboardInfo:Show();
 				button.Details:Show();
 				button.highlight:Show();
-				local details =  table.concat(info:GetDetailedOutputLines(), "\n", 2)
+				local details = "<html><body><p>";
+				details = details .. table.concat(info:GetDetailedOutputLines(), "<br/>", 2)
 				details = details:gsub(DETAILS_NO_PUBLIC, DETAILS_NO_PUBLIC_REPLACE);
 				
+				details = details .. "</p></body></html>"
+				
+				button.Details:SetHyperlinksEnabled(true);
 				button.Details:SetText(details);
-				button:SetHeight(LISTITEM_HEIGHT + LISTITEM_EXPAND_MARGIN + button.Details:GetHeight());
+				heightStuff = LISTITEM_HEIGHT + LISTITEM_EXPAND_MARGIN + button.Details:GetContentHeight();
+				
+				button:SetHeight(heightStuff);
+				HybridScrollFrame_ExpandButton(APIIListsSystemList, ((displayIndex-1) * LISTITEM_HEIGHT), heightStuff);
 			end
 		else
 			button:Hide();
 		end
 	end
 	
-	local extra = APIIListsSystemList.largeButtonHeight or LISTITEM_HEIGHT 
+	
+	
+	local extra = APIIListsSystemList.largeButtonHeight or heightStuff;
 	local totalHeight = #list * LISTITEM_HEIGHT
 	totalHeight = totalHeight + (extra - LISTITEM_HEIGHT)
+	
 	HybridScrollFrame_Update(scrollFrame, totalHeight, scrollFrame:GetHeight());
+
 end
 
 function APII:OnInitialize()
@@ -372,10 +413,12 @@ function APII:OnEnable()
 	APIIListsSystemListScrollBar.doNotHide = true;
 	APIIListsSystemListScrollBar:Show();
 	
-	APIIListsSystemList.update = function() APII:UpdateSystemList() end;
+	APIIListsSystemList.update = function() 
+				APII:UpdateSystemList();
+		end;
 	APIIListsSystemList.SearchString = "";
 	APIIListsSystemList.SearchResults = {};
-	APIIListsSystemList.Opened = {};
+	APIIListsSystemList.Opened = "";
 	APII:UpdateSystemList();
 end
 
